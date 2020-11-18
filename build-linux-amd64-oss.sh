413d58d5bf6160bd55ffae1fbc6d5f3c0758969a
Yuriy Musatenko <yuriy@prismskylabs.com>
2020-11-18 11:38:01 -0800

    Adding extra boilerplate code to avoid its loss

--- build-linux-amd64-oss.sh
+++ build-linux-amd64-oss.sh
@@ -8,6 +8,14 @@ TEST_BUILD_DIR=$ROOT_DIR/build
 
 [ -d $TEST_BUILD_DIR ] && rm -Rf $TEST_BUILD_DIR/* || mkdir -p $TEST_BUILD_DIR
 conan remote list
+
+# Code in 'if' below is needed only if your lib has required dependencies.
+if [ "$CONAN_USER_INFO" == "Current user of remote 'psl-conan' set to: 'None' (anonymous)" ]; then
+    echo "Logging into psl-conan server."    
+    echo "Please, enter psl-conan (artifactory-cpp) credentials when prompted"    
+    conan user -r psl-conan  -p    
+fi
+
 CONAN_PROF=$ROOT_DIR/src/external/edge_build_bootstrap/src/platforms/x86/linux/linux-x86_64-release.pr
 conan create $ROOT_DIR   -tbf $TEST_BUILD_DIR --profile $CONAN_PROF 
 
