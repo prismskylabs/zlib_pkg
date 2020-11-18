b7bbf214131970f030be3d215f912d1572391ca9
Yuriy Musatenko <yuriy@prismskylabs.com>
2020-11-17 18:09:55 -0800

    Bugfix

--- conanfile.py
+++ conanfile.py
@@ -43,7 +43,7 @@ class ZlibConan(ConanFile):
             self._build_minizip()
 
     def conan_info(self): # https://github.com/conan-io/conan/issues/212
-        if self.settings.compiler == "gcc" or self.settings.compiler == "clang"::  # or3 self.settings.os != "Windows" or the like
+        if self.settings.compiler == "gcc" or self.settings.compiler == "clang":  # or3 self.settings.os != "Windows" or the like
             self.info.settings.build_type = None # Make gcc to be all Release packages linkable to Debug too
 
     @property
